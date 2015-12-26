509a25a70d76ee6cbe11f7422498b2e55d859c1c
Tuxerr <tuxerr.at@gmail.com>
2015-12-26 17:47:15 +0100

    Added command line options

--- blatt09/main.c
+++ blatt09/main.c
@@ -1,5 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "output.h"
 
-void main(int argc, char** argv){
-    output_image(2.5, 2.5, 5, 1200, 800, 1000);
+char* usage =  "Usage: [-x Re(c)] [-y Im(c)] [-d Re(cmax -cmin)] [-w width] [-h height] [-i maximal iteration count]\n";
+
+int main(int argc, char** argv){
+    int c;
+    long double x = 2.5;
+    long double y = 2.5;
+    long double d = 5;
+    int w = 800;
+    int h = 600;
+    int i = 200;
+    while((c = getopt(argc, argv, "x:y:w:h:d:i:")) != -1) {
+        switch(c) {
+            case 'x':
+                x = atof(optarg);
+                break;
+            case 'y':
+                y = atof(optarg);
+                break;
+            case 'd':
+                d = atof(optarg);
+                break;
+            case 'w':
+                w = atoi(optarg);
+                break;
+            case 'h':
+                h = atoi(optarg);
+                break;
+            case 'i':
+                i = atoi(optarg);
+                break;
+            case ':':
+                switch(optopt) {
+                    case '?':
+                        printf(usage);
+                        return  1;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                printf(usage);
+                return 1;
+        }
+    }
+    output_image(x, y, d, w, h, i);
 }
--- blatt09/output.c
+++ blatt09/output.c
@@ -1,4 +1,11 @@
+#include <math.h>
 #include "output.h"
+
+/*void blend_colors(guchar *a, guchar *b, float a_percent) {
+    b[0] = sqrt(a_percent*a[0]*a[0] + (1 - a_percent)*b[0]*b[0]);
+    b[1] = sqrt(a_percent*a[1]*a[1] + (1 - a_percent)*b[1]*b[1]);
+    b[2] = sqrt(a_percent*a[2]*a[2] + (1 - a_percent)*b[2]*b[2]);
+}*/
 
 void output_image (long double x, long double y, long double hwidth, int width, int height, int iter_count) {
     GdkPixbuf* image = gdk_pixbuf_new(GDK_COLORSPACE_RGB, FALSE, 8, width, height);
@@ -11,14 +18,24 @@ void output_image (long double x, long double y, long double hwidth, int width, 
             long double _Complex c = (long double) j / (long double) width * hwidth - x + ((long double) i / (long double) height * hwidth * height / width - y ) * I;
             int max_iter_count = cabsl(c) >= 2 ? 1 : get_iter_count(c, iter_count);
             if(!max_iter_count) {
-                pp[0] = 255;
+                pp[0] = 0;
                 pp[1] = 0;
                 pp[2] = 0;
             } else {
-                pp[0] = (float) max_iter_count / (float) iter_count * 255;
+                pp[0] = ((float) max_iter_count / (float) iter_count) * 255;
                 pp[1] = 0;
-                pp[2] = 255 - (float) max_iter_count / (float) iter_count * 255;
+                pp[2] = 255 - ((float) max_iter_count / (float) iter_count) * 255;
             }
+            /*if(j > 3&& i > 3 && j < width - 3 && i < height - 3) {
+                for (int k = -3; k <= 3; k++) {
+                    for (int l = -3; l <= 3; l++) {
+                        if((k == 0 && l ==0) || sqrt(k*k + l*l) > 3) {
+                            continue;
+                        }
+                        blend_colors(pp, pixels + (i + k) * rowstride + (j + l) * n_channels, (4 - fabs(sqrt(k*k + l*l))) / 4.0);
+                    }
+                }
+            }*/
         }
     }
     gdk_pixbuf_save(image, "fractal.png", "png", NULL, NULL, NULL);
